a35574eb40cb4db7f5aeab725651e83be015a92b
roguesmoke <smokiem@outlook.com>
2019-05-24 12:41:54 +0200

    Update inschrijvingen.sql
    opdracht 1 update

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -2,7 +2,7 @@ USE lo8e_sql2;
 
 INSERT INTO klassen
     (klas_code, slb_code, cohort, opleiding_code) VALUES 
-	('lo9e-amo1','vlt03', 2019, 'amo'), 
+	('lo9e-amo1','brc01', 2019, 'amo'), 
 	('lo9e-amo2','rs001', 2019, 'amo'), 
     ('lo9e-amo3','akr02', 2019, 'amo'); 
 
@@ -11,3 +11,10 @@ INSERT INTO studenten (ov_nummer, achternaam, tussenvoegsel, voornaam, geboorted
 	(90001,'Adje','','Loss','1993-04-22','2213 GS','Noordsingel', 80.3, 1.82, '2018-01-01'),
 	(90002,'Tien','','Erik','1993-05-23','2214 TT','Westeinde', 75.5, 1.85, '2018-01-01'),
 	(90003,'Bob','','Martijn','1993-06-24','2215 ZZ','Oosteinde', 76.7, 1.84, '2018-01-01');
+
+INSERT INTO klassen_studenten(ov_nummer, klas_code) VALUES
+	(90000,'lo9e-amo1'),
+	(90001,'lo9e-amo1'),
+	(90002,'lo9e-amo2'),
+	(90003,'lo9e-amo3');
+
